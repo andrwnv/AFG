feb6b9a01a8e051b3db4364f9fddd12a86c4a7d6
Andrew G <glazynovand@gmail.com>
2020-05-22 01:12:03 +0700

    fix button press in nemu component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import StartMenu from './components/startMenu';
+// import StartMenu from './components/startMenu';
 
 import MenuButton from './components/MenuBottom';
 import { View } from 'react-native';
--- components/MenuBottom.tsx
+++ components/MenuBottom.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Dimensions ,StyleSheet, Image, View, TouchableOpacity } from 'react-native';
+import { Dimensions ,StyleSheet, Image, View, TouchableOpacity, TouchableOpacityBase } from 'react-native';
 
 /*
-
-    @brief: 
-
+*
+    @brief: Element of game control for change loactions.
+*
 */
 
 export default class MenuBottom extends Component {
@@ -15,36 +15,59 @@ export default class MenuBottom extends Component {
 
     state = { button: true };
 
-    handleClick = (): void => {
-        console.log('IDK WTF');
-        this.setState({
-            button: !this.state.button
-        });
+    handleClick = (icon: any): void => {
+        console.log(this.icons)
+
+        this.icons.cat.pressed  = false;
+        this.icons.vat.pressed  = false;
+        this.icons.hat.pressed  = false;
+        this.icons.home.pressed = false;
+        this.icons.dirt.pressed = false;
+
+        icon.pressed = true;
+
+        this.forceUpdate();
     };
+
+    icons = { home : { link: require('../assets/menu/Home.png'), pressed: true },
+              vat  : { link: require('../assets/menu/Vat.png'),  pressed: false },
+              cat  : { link: require('../assets/menu/Cat.png'),  pressed: false },
+              hat  : { link: require('../assets/menu/Hat.png'),  pressed: false },
+              dirt : { link: require('../assets/menu/Dirt.png'), pressed: false } };
 
     render(): JSX.Element {
         return (
             <View style = {styles.BottomMenu} >
                  <View style = {styles.Menu} >
-                    <TouchableOpacity onPress = {this.handleClick} style={this.state.button ? styles.defaultButton:styles.pressedButton}>
-                        <Image source={require('../assets/menu/Home.png')} style={this.state.button ? styles.imageDefaultColor:styles.imagePressedColor} />
-                    </TouchableOpacity >
+                    <TouchableOpacity onPress = {() => {this.handleClick(this.icons.home)}} 
+                                      style={this.icons.home.pressed ? styles.pressedButton:styles.defaultButton}>
+                        <Image  source={this.icons.home.link} 
+                                style={this.icons.home.pressed ? styles.imageDefaultColor:styles.imagePressedColor} />
+                    </TouchableOpacity>
 
-                    <TouchableOpacity onPress={this.handleClick} style={this.state.button ? styles.defaultButton:styles.pressedButton}>
-                        <Image  source={require('../assets/menu/Vat.png')} style={this.state.button ? styles.imageDefaultColor:styles.imagePressedColor}/>
-                    </TouchableOpacity >
+                    <TouchableOpacity onPress={() => {this.handleClick(this.icons.vat)}} 
+                                      style={this.icons.vat.pressed ? styles.pressedButton:styles.defaultButton}>
+                        <Image  source={this.icons.vat.link} 
+                                style={this.icons.vat.pressed ? styles.imageDefaultColor:styles.imagePressedColor}/>
+                    </TouchableOpacity>
                     
-                    <TouchableOpacity onPress={this.handleClick} style={this.state.button ? styles.defaultButton:styles.pressedButton}>
-                        <Image  source={require('../assets/menu/Cat.png')} style={this.state.button ? styles.imageDefaultColor:styles.imagePressedColor} />
-                    </TouchableOpacity >
+                    <TouchableOpacity onPress={() => {this.handleClick(this.icons.cat)}} 
+                                      style={this.icons.cat.pressed ? styles.pressedButton:styles.defaultButton}>
+                        <Image  source={this.icons.cat.link} 
+                                style={this.icons.cat.pressed ? styles.imageDefaultColor:styles.imagePressedColor} />
+                    </TouchableOpacity>
 
-                    <TouchableOpacity onPress={this.handleClick} style={this.state.button ? styles.defaultButton:styles.pressedButton}>
-                        <Image  source={require('../assets/menu/Hat.png')} style={this.state.button ? styles.imageDefaultColor:styles.imagePressedColor} />
-                    </TouchableOpacity >
+                    <TouchableOpacity onPress={() => {this.handleClick(this.icons.hat)}} 
+                                      style={this.icons.hat.pressed ? styles.pressedButton:styles.defaultButton}>
+                        <Image  source={this.icons.hat.link} 
+                                style={this.icons.hat.pressed ? styles.imageDefaultColor:styles.imagePressedColor} />
+                    </TouchableOpacity>
 
-                    <TouchableOpacity onPress={this.handleClick} style={this.state.button ? styles.defaultButton:styles.pressedButton}>
-                        <Image  source={require('../assets/menu/Dirt.png') } style={this.state.button ? styles.imageDefaultColor:styles.imagePressedColor} />
-                    </TouchableOpacity >
+                    <TouchableOpacity onPress={() => {this.handleClick(this.icons.dirt)}} 
+                                      style={this.icons.dirt.pressed ? styles.pressedButton:styles.defaultButton}>
+                        <Image  source={this.icons.dirt.link} 
+                                style={this.icons.dirt.pressed ? styles.imageDefaultColor:styles.imagePressedColor} />
+                    </TouchableOpacity>
                 </View>
             </View>
         );
