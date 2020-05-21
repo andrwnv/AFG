6d0914d97ea1afb50f1368f19500a6fafb31f4fe
Andrew G <glazynovand@gmail.com>
2020-05-21 14:56:20 +0700

    rename component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import StartMenu from './components/startMenu';
 
-import MenuButton from './components/Menu_Bottom';
+import MenuButton from './components/MenuBottom';
 import { View } from 'react-native';
 
 export default function App() {
