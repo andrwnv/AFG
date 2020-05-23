cef7f6f55d09816427243536a6fc3994633af145
Andrew G <glazynovand@gmail.com>
2020-05-24 01:53:35 +0700

    add handler container

--- components/MenuBottom.tsx
+++ components/MenuBottom.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Dimensions ,StyleSheet, Image, View, TouchableOpacity } from 'react-native';
+import { Dimensions ,StyleSheet, Image, View, TouchableOpacity, Alert } from 'react-native';
 
 /*
 *
@@ -15,34 +15,37 @@ export default class MenuBottom extends Component {
 
     state = { button: true };
 
-    handleClick = (icon: any): void => {
-        
-        this._icons.cat.pressed  = false;
-        this._icons.vat.pressed  = false;
-        this._icons.hat.pressed  = false;
-        this._icons.home.pressed = false;
-        this._icons.dirt.pressed = false;
+    _handlers: any[] = [];
 
-        icon.pressed = true;
+    handleClick = (icon: any): void => {
+        for (let [, value] of Object.entries(this._icons)) {
+            value.pressed = false;
+        }
+        
+        // TODO(andrew): find icon index + call _hanlder[index](...);
 
         this.forceUpdate();
     };
 
     _icons = { home : { link: require('../assets/menu/Home.png'), pressed: true },
-              vat  : { link: require('../assets/menu/Vat.png'),  pressed: false },
-              cat  : { link: require('../assets/menu/Cat.png'),  pressed: false },
-              hat  : { link: require('../assets/menu/Hat.png'),  pressed: false },
-              dirt : { link: require('../assets/menu/Dirt.png'), pressed: false } };
+               vat  : { link: require('../assets/menu/Vat.png'),  pressed: false },
+               cat  : { link: require('../assets/menu/Cat.png'),  pressed: false },
+               hat  : { link: require('../assets/menu/Hat.png'),  pressed: false },
+               dirt : { link: require('../assets/menu/Dirt.png'), pressed: false } };
 
     render(): JSX.Element {
         let iconsArray: JSX.Element[] = [];
 
-        for (let [, value] of Object.entries(this._icons)) {
-            iconsArray.push(<TouchableOpacity onPress={() => {this.handleClick(value)}} 
-                                          style={value.pressed ? styles.pressedButton:styles.defaultButton}>
-                            <Image  source={value.link} 
-                                    style={value.pressed ? styles.imageDefaultColor:styles.imagePressedColor} />
-                        </TouchableOpacity>);
+        for (let [key, value] of Object.entries(this._icons)) {
+            iconsArray.push(<TouchableOpacity key={key} onPress={() => {this.handleClick(key)}} 
+                                              style={value.pressed ? styles.pressedButton : styles.defaultButton}>
+                                    <Image source={value.link} key = {key}
+                                           style={value.pressed ? styles.imageDefaultColor : styles.imagePressedColor} />
+                            </TouchableOpacity>);
+            
+            this._handlers.push(() => {
+                Alert.alert('Hanlder of icon');
+            });
         }
 
         return (
@@ -92,8 +95,8 @@ const styles = StyleSheet.create({
         width: 59,
         height: 59,
         borderRadius: 200,
-        marginLeft: 8,
-        marginRight: 8,
+        marginLeft: 10,
+        marginRight: 10,
         marginTop: 15,
         backgroundColor: '#EE8AF0'
     },
@@ -104,8 +107,8 @@ const styles = StyleSheet.create({
         width: 59,
         height: 59,
         borderRadius: 200,
-        marginLeft: 8,
-        marginRight: 8,
+        marginLeft: 10,
+        marginRight: 10,
         marginTop: 15,
         backgroundColor: 'rgba(251, 201, 255, 0.85)'
     },
