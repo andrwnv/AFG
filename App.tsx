ca855d7c2a04b917331ecf2bf17c904ec1ff2eb5
Andrew G <glazynovand@gmail.com>
2020-06-03 17:44:50 +0300

    connect firestore

--- App.tsx
+++ App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
 import StartMenu from './components/startMenu';
 
-export default function App() {
-  return (
-    <StartMenu/>
+import { firebaseConfig } from './api/ApiKeys';
+
+import * as firebase from 'firebase';
+
+export default class App extends Component {
+  constructor(props: any) {
+    super(props);
+ }
+
+  componentDidMount() {
+    if (! firebase.apps.length) { firebase.initializeApp(firebaseConfig); } 
+    else { console.log('wtf'); }
+    
+    let dbh = firebase.firestore(); 
+
+    console.log(firebase);
+    
+    dbh.collection('users_props').get()
+    .then((snapshot: any) => {
+      snapshot.forEach((doc: any) => {
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch((err: any) => {
+      console.log('Error getting documents', err);
+    });
+  }
+
+  render () {
+    return (
+      <StartMenu/>
   );
+  }
 }
