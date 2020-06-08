961b65fa4befb0b977c822b6c02581d677bb6c81
Andrew G <glazynovand@gmail.com>
2020-06-09 01:53:06 +0300

    delete useless comments

--- App.tsx
+++ App.tsx
@@ -10,15 +10,8 @@ export default class App extends Component {
 
     componentDidMount() {
         // const firestore = new FirestoreAPI();
-        // firestore.getUsers().then( (value) => { console.log(value); });
-        // firestore.setUserFields('asdfghjkl', {tes: "qwertyu", test: 'adsdasd'}).then( value => {console.log(value)} )
-        // firestore.getUserFields('idkasdasdasd');
-
-        // firestore.createUser('some_user_228').then((res) => {console.log(res)});
-
-        const firestore = new FirestoreAPI();
-        firestore.createUser('+79991774634');
-        firestore.getUsers().then(val => { console.log(val); })
+        // firestore.createUser('+79991774634');
+        // firestore.getUsers().then(val => { console.log(val); })
     }
 
     render () {
