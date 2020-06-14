c5903015be36c6b68a39535d68d6eee416f71151
Andrew G <glazynovand@gmail.com>
2020-06-15 00:49:53 +0300

    delete start menu from bottom menu branch

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-// import StartMenu from './components/startMenu';
+import { View } from 'react-native';
 
 import MenuButton from './components/MenuBottom';
-import { View } from 'react-native';
 
 export default function App() {
   return (
     <View>
         <MenuButton/>
-        {/* <StartMenu/> */}
     </View>
     
   );
