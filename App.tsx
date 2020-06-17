4b9142e64c14e21c554715b778682dc3c8f788c6
Diklofozzz1 <vova-nagornyj@mail.ru>
2020-06-17 23:32:12 +0700

    unworking sms auth

--- App.tsx
+++ App.tsx
@@ -4,22 +4,26 @@ import React, { Component } from "react";
 
 import MenuButton from './components/MenuBottom/MenuBottom';
 import FirestoreAPI from './api/FirestoreAPI';
+import phoneSingIn from './api/SMS_AUTH/TestSMSAuth'
 
 export default class App extends Component {
     constructor(props: any) {
         super(props);
     }
 
+
     componentDidMount() {
         // const firestore = new FirestoreAPI();
         // firestore.createUser('+7999*******');
         // firestore.getUsers().then(val => { console.log(val); })
+    //    phoneSingIn();
     }
 
     render () {
         return (
           <View>
-              <MenuButton/>
+            {phoneSingIn()}
+              {/* <MenuButton/> */}
           </View>
       );
     }
