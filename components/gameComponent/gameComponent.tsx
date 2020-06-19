8519871cdb189bcc3e4866ba6a0a48ffaa817775
Andrew G <glazynovand@gmail.com>
2020-06-19 14:00:07 +0300

    add main game component

---
+++ components/gameComponent/gameComponent.tsx
@@ -0,0 +1,22 @@
+import { ImageBackground, Image } from 'react-native';
+import React, { Component } from "react";
+
+// View components.
+import MenuButton    from '../MenuBottom/MenuBottom';
+import HeroStatusBar from '../HeroStatusBar/HeroStatusBar';
+
+export default class App extends Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+          return (
+                <ImageBackground source={require('./assets/backgrounds/BedRoom.png')}> 
+                        <HeroStatusBar handler={(): void => console.log('helloooooooo')} />
+                        <Image source={require('./assets/sprites/AsunaDefault.png')}/>
+                        <MenuButton/>
+                </ImageBackground>
+        );
+    }
+}
