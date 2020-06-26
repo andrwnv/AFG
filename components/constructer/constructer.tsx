002c5e51ae1858a24d4274bd44376daf2827e435
Andrew G <glazynovand@gmail.com>
2020-06-27 00:08:40 +0300

    add create button

--- components/constructer/constructer.tsx
+++ components/constructer/constructer.tsx
@@ -88,6 +88,11 @@ export default class StartMenu extends Component {
                         </TouchableOpacity>
                     </View>
                 </View>
+                <View style={{position: 'absolute', top: width * 1.5, width: '100%', height: '100%'}}> 
+                        <TouchableOpacity style={{backgroundColor: '#E76BC0', borderRadius: 10, width: '90%', height: '10%', justifyContent: 'center', alignItems: 'center', marginBottom: 10, marginLeft: 'auto', marginRight: 'auto'}}> 
+                            <Text style={{color: 'white', fontFamily: 'Montserrat-Medium', fontSize: 17}}>Создать</Text>
+                        </TouchableOpacity>
+                    </View>
             </View>
 
         </KeyboardAvoidingView>
