f3cf3eedc4f21fdcaeb4053052156f7b3f151aef
Diklofozzz1 <vova-nagornyj@mail.ru>
2020-07-07 16:42:46 +0700

    create folder for game...

---
+++ components/cookingGame/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+const colorSpectrum = {
+    white: '#ffffff',
+    pink:  '#FBC9FF',
+    lightPink:  '#F57CFF',
+    darkPink: '#C19AC5',
+    alternativPink: '#B72FA9',
+    black: '#000000',
+    orange:'#FF7A00',
+    red:'#C34A08'
+}
+
+
+export const styles = StyleSheet.create({
+    content: {
+        backgroundColor: colorSpectrum.white,
+        flex: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+})
