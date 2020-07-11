b3a60456b6502f0df4076be791f4369cd45edaae
Andrew G <glazynovand@gmail.com>
2020-07-12 00:06:23 +0300

    now u can throw dart when u click anywhere on screen

--- Routes.tsx
+++ Routes.tsx
@@ -2,23 +2,25 @@ import { Router, Scene } from 'react-native-router-flux';
 import React from 'react';
 
 import CharacterConstructor from './components/HeroConstructer/HeroConstucter';
-import RegistrationMenu     from './components/RegistrationMenu/RegistrationMenu'
-import CreateCharacter      from './components/CreationCharacterMenu/CreationCharacterMenu';
+import RegistrationMenu     from './components/registrationMenu/RegistrationMenu';
+import CreateCharacter      from './components/creationCharacterMenu/CreationCharacterMenu';
 import ShopComponent        from './components/ShopComponent/ShopComponent';
-import GameComponent        from './components/GameComponent/GameComponent';
-import CharacterMenu        from './components/CharacterMenu/CharacterMenu';
-import StartMenu            from './components/StartMenu/StartMenu';
+import GameComponent        from './components/gameComponent/GameComponent';
+import CharacterMenu        from './components/characterMenu/CharacterMenu';
+import StartMenu            from './components/startMenu/StartMenu';
+import Darts                from "./components/darts/Darts";
 
 const Routes = () => (
    <Router>
       <Scene key = "root">
          <Scene key = "LogIn"                component = {StartMenu}            hideNavBar = {true} title = "LogIn" initial = {true} />
-         <Scene key = "SigIn"                component = {RegistrationMenu}     hideNavBar = {true} title = "SignIn" />
+         <Scene key = "SignIn"               component = {RegistrationMenu}     hideNavBar = {true} title = "SignIn" />
          <Scene key = "CharacterMenu"        component = {CharacterMenu}        hideNavBar = {true} title = "HeroMenu" />
          <Scene key = "CharacterConstructor" component = {CreateCharacter}      hideNavBar = {true} title = "Construct" />
          <Scene key = "CreateCharacter"      component = {CharacterConstructor} hideNavBar = {true} title = "Create" />
          <Scene key = "GameComponent"        component = {GameComponent}        hideNavBar = {true} title = "Game" />
          <Scene key = "Shop"                 component = {ShopComponent}        hideNavBar = {true} title = "Shop" />
+         <Scene key = "Darts"                component = {Darts}                hideNavBar = {true} title = "Darts" />
       </Scene>
    </Router>
 );
--- components/darts/Darts.tsx
+++ components/darts/Darts.tsx
@@ -36,28 +36,28 @@ export default class Darts extends Component {
     return (
       <View>
         <View>
-          <ImageBackground source={require('./assets/Back.png')} style={{width: Constants.MAX_WIDTH, height: Constants.MAX_HEIGHT}}>
-            <View style = {{justifyContent: "center"}}>
-              <TouchableOpacity onPress={() => { this.engine.dispatch({type: 'next-state'})}} activeOpacity = {1}>
-                <ImageBackground source={require('./assets/Board.png')} style={{flexDirection: 'column', width: Constants.MIN_SIDE *0.9, height: Constants.MIN_SIDE *0.9, left: Constants.MIN_SIDE * 0.05, top: Constants.MAX_HEIGHT * 0.2 }}>
-                  <GameEngine
-                    ref = {(ref) => {this.engine = ref}}
-                    systems = {[ GameLoop ]}
-                    entities = {{
+          <TouchableOpacity onPress={() => { this.engine.dispatch({type: 'next-state'})}} activeOpacity = {1}>
+            <ImageBackground source={require('./assets/Back.png')} style={{width: Constants.MAX_WIDTH, height: Constants.MAX_HEIGHT}}>
+              <View style = {{justifyContent: "center"}}>
+                  <ImageBackground source={require('./assets/Board.png')} style={{flexDirection: 'column', width: Constants.MIN_SIDE *0.9, height: Constants.MIN_SIDE *0.9, left: Constants.MIN_SIDE * 0.05, top: Constants.MAX_HEIGHT * 0.2 }}>
+                    <GameEngine
+                      ref = {(ref) => {this.engine = ref}}
+                      systems = {[ GameLoop ]}
+                      entities = {{
 
-                      aim: {position: [25,25], speed: 1, xDirection: true,  yDirection: true, state: 0 ,size: Constants.DART_SIZE, renderState: false , renderer: <Aim/>},
-                      points: {score: 0, counting: true},
-                      dart: {elements: [], renderer: <Dart/>}
-                    }}
+                        aim: {position: [25,25], speed: 1, xDirection: true,  yDirection: true, state: 0 ,size: Constants.DART_SIZE, renderState: false , renderer: <Aim/>},
+                        points: {score: 0, counting: true},
+                        dart: {elements: [], renderer: <Dart/>}
+                      }}
 
-                    onEvent  = {this.onEvent}
-                    running = {this.state.running}
-                  />
-                </ImageBackground>
-              </TouchableOpacity>
-            </View>
-          </ImageBackground>
-        </View> 
+                      onEvent  = {this.onEvent}
+                      running = {this.state.running}
+                    />
+                  </ImageBackground>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+        </View>
       </View> 
     );
   }
