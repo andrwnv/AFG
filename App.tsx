c6000b16c35d8984c496f09f04f5799bc1232394
Andrew G <glazynovand@gmail.com>
2020-07-24 00:44:51 +0300

    fix code style

--- App.tsx
+++ App.tsx
@@ -2,17 +2,11 @@ import { View, StatusBar, Dimensions } from 'react-native';
 import React, { Component } from "react";
 import * as Font from 'expo-font';
 
-
-//import Duck from "./components/games/ducks/Duck";
-//import Hf from "./components/games/headphones/headphones";
-
-
 import Routes from './Routes';
 
 // @ts-ignore
 import awsconfig from './aws-exports';
 import Amplify from 'aws-amplify';
-import { Router } from 'react-native-router-flux';
 
 Amplify.configure(awsconfig);
 
@@ -27,27 +21,26 @@ export default class App extends Component {
 
     state = { fontLoaded: false };
 
-    componentDidMount = async() => {
-      await Font.loadAsync({
-          'Montserrat-Regular'  :require('./assets/fonts/Montserrat-Regular.ttf'),
-          'Montserrat-Black'    :require('./assets/fonts/Montserrat-Black.ttf'),
-          'Montserrat-Medium'   :require('./assets/fonts/Montserrat-Medium.ttf'),
-          'Montserrat-Light'    :require('./assets/fonts/Montserrat-Light.ttf'),
-          'Montserrat-Italic'   :require('./assets/fonts/Montserrat-Italic.ttf'),
-          'Montserrat-Thin'     :require('./assets/fonts/Montserrat-Thin.ttf'),
-          'Montserrat-SemiBold' :require('./assets/fonts/Montserrat-SemiBold.ttf'),
-          'Montserrat-Bold'     :require('./assets/fonts/Montserrat-Bold.ttf')
-      });
+    async componentDidMount () {
+          await Font.loadAsync({
+              'Montserrat-Regular'  :require('./assets/fonts/Montserrat-Regular.ttf'),
+              'Montserrat-Black'    :require('./assets/fonts/Montserrat-Black.ttf'),
+              'Montserrat-Medium'   :require('./assets/fonts/Montserrat-Medium.ttf'),
+              'Montserrat-Light'    :require('./assets/fonts/Montserrat-Light.ttf'),
+              'Montserrat-Italic'   :require('./assets/fonts/Montserrat-Italic.ttf'),
+              'Montserrat-Thin'     :require('./assets/fonts/Montserrat-Thin.ttf'),
+              'Montserrat-SemiBold' :require('./assets/fonts/Montserrat-SemiBold.ttf'),
+              'Montserrat-Bold'     :require('./assets/fonts/Montserrat-Bold.ttf')
+          });
 
       this.setState( { fontLoaded: true } );
-  }
+    }
 
     render () {
-      if (this.state.fontLoaded) {
-          return ( <Routes/> );
-      } else {
-        return ( <View/> );
-      }
+          if (this.state.fontLoaded) {
+                return ( <Routes/> );
+          } else {
+                return ( <View/> );
+          }
     }
-   
 }
