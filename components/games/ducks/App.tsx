58e0154d164de8ee79d3ac63a0f53ac94e00ee1a
Andrew G <glazynovand@gmail.com>
2020-07-24 00:15:35 +0300

    fix ducks game

--- components/games/ducks/App.tsx
+++ components/games/ducks/App.tsx
@@ -2,76 +2,68 @@ import React, {Component} from 'react';
 
 import { GameEngine } from "react-native-game-engine";
 
-
-import { Dimensions, StyleSheet, StatusBar,  Text, View , TouchableOpacity, Alert, Button, Image, ImageBackground, Animated, Swipese } from 'react-native';
+import { View, Alert,ImageBackground } from 'react-native';
 import { Constants } from './Constants';
 import { GameLoop } from './GameLoop';
 import Projectile from './Projectile';
-import Slingshot from './Slingshot'; 
-import Duck from './Duck'; 
+import Slingshot from './Slingshot';
+import Duck from './Duck';
 
-export default class DF extends React.Component {
+export default class DF extends Component {
+    boardSize: number;
+    engine: any;
+    props: any;
 
-  constructor(props){
-    super(props);
-    this.boardSize = Constants.MIN_SIDE;
-    this.engine = null;
-    this.state = {
-      running: true,
-
+    state = {
+       running: true,
     }
-    
-  }
 
-  
-  
-  onEvent = (e) => {
-    if (e.type === "game-over"){
-      //Alert.alert("Game Over");
-      alert("Game Over");   
-      this.setState({
-        running: false
-      })   
+    constructor(props: any){
+        super(props);
+
+        this.props = props;
+        this.boardSize = Constants.MIN_SIDE;
+        this.engine = null;
     }
+
+  onEvent = (e: any) => {
+      if (e.type === "game-over"){
+          Alert.alert("Game Over");
+          this.setState({
+             running: false
+          })
+      }
   }
 
-  randomBetween = (min, max) => {
-    return Math.floor(Math.random() *  (max - min + 1) + min);
-  }
-  
+    randomBetween = (min: number, max: number) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
 
   render() {
-    return (
-      <View>
-        <View>
-          <ImageBackground source={require('./Back.png')} style={{width: Constants.MAX_WIDTH, height: Constants.MAX_HEIGHT}}>
-            <View style = {{justifyContent: "center"}}>
-              <GameEngine
-                ref = {(ref) => {this.engine = ref}}
+      return (
+          <View>
+              <View>
+                  <ImageBackground source={require('./Back.png')} style={{width: Constants.MAX_WIDTH, height: Constants.MAX_HEIGHT}}>
+                      <View style = {{justifyContent: "center", width: Constants.MAX_WIDTH, height: Constants.MAX_HEIGHT}}>
+                        <GameEngine
+                            ref = {(ref) => {this.engine = ref}}
+                            systems = {[ GameLoop ]}
+                            entities = {{
+                              projectile: {position: {x : 25, y : 75} , launch: false, speed: {x : 0, y : 0}, startPos:  {x : 0, y : 0}, endPos:  {x : 0, y : 0}, deg: 0, hited : false, missed : false , amount : 3, renderer :  <Projectile/>},
+                              slingshot: {position: {x : 25, y : 75}, renderer :  <Slingshot/>},
+                              duck: { position: {x : this.randomBetween(8,Constants.GRID_SIZE - 9),y : this.randomBetween(8,Constants.GRID_SIZE - 9)}, speed: {x : 0.3, y : 0.15},startPos: {x : 0, y :0, filled: false}, dir: {x : true, y : true}, renderer:  <Duck/>},
+                              points: {score: 0, counting: true}  ,
+                            }}
 
-                style={{width: Constants.MAX_WIDTH, height: Constants.MAX_HEIGHT, flex: null}}
+                            onEvent  = {this.onEvent}
+                            running = {this.state.running}/>
+                      </View>
+                  </ImageBackground>
 
-                systems = {[ GameLoop ]}
+              </View>
+          </View>
+      )
 
-                entities = {{
-                  projectile: {position: {x : 25, y : 75} , launch: false, speed: {x : 0, y : 0}, startPos:  {x : 0, y : 0}, endPos:  {x : 0, y : 0}, deg: 0, hited : false, missed : false , amount : 3, renderer :  <Projectile/>},
-                  slingshot: {position: {x : 25, y : 75}, renderer :  <Slingshot/>},
-                  duck: { position: {x : this.randomBetween(8,Constants.GRID_SIZE - 9),y : this.randomBetween(8,Constants.GRID_SIZE - 9)}, speed: {x : 0.3, y : 0.15},startPos: {x : 0, y :0, filled: false}, dir: {x : true, y : true}, renderer:  <Duck/>},
-                  points: {score: 0, counting: true}  ,
-                  //test : {  position: {x : 22, y : 25}, renderer :  <Test/>}                    
-                }}
-
-                onEvent  = {this.onEvent}
-                running = {this.state.running}
-              >
-              
-              </GameEngine> 
-            </View>
-          </ImageBackground>
-          
-        </View>
-      </View> 
-    )
-        
-    }
-  } 
+  }
+}
