15b13430da6c1b730b4c53bbc184678cc519989a
Andrew G <glazynovand@gmail.com>
2020-07-24 15:43:39 +0300

    [Headphones game]: go to ts (finish)

---
+++ components/games/headphones/Thread.tsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import {View} from "react-native";
+import Svg, { Line } from "react-native-svg";
+
+export default class Node extends Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <View>
+                <Svg>
+                    <Line x1="0" y1="0" x2="100" y2="100" stroke="red" strokeWidth="2" />
+                </Svg>   
+            </View>
+        );
+    }
+}
