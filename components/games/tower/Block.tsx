6c9bea46e69e2f9b86439385450e8b9f42c746b5
Glazynov A <glazunov_ay@lemz-t.ru>
2020-08-05 13:00:28 +0700

    [Tower game]: go to ts

--- components/games/tower/Block.tsx
+++ components/games/tower/Block.tsx
@@ -4,34 +4,38 @@ import { View }             from 'react-native';
 import { Constants }        from './Constants';
 
 
-interface Interface {
+interface InterfaceTest {
     elements: any[],
     n: number
 }
 
-export default class Block extends Component<Interface> {
+export default class Block extends Component<InterfaceTest> {
+    props: any;
+    
     constructor(props: any) {
         super(props);
+        this.props = props;
     }
 
-    a() {
-        let blockList: any[] = [];
-        let i: number = 0;
-
+    a(index: number) {
         let list = [];
 
-        for (let j = 0; j < this.props.elements[i].length; j++){
-            list.push(this.props.elements[i][j]);
+        for (let j = 0; j < this.props.elements[index].length; j++){
+            list.push(this.props.elements[index][j]);
         }
+        
+        let blockList: JSX.Element[] = [];
 
         blockList = list.map((el, idx) => {
+            const red = 255 - (el.width - 8) / 20 * 255;
+            const blue = (el.width - 8) / 20 * 255;
             return (
-                <View key = {idx + i*this.props.n} style = {{
+                <View key = {idx + index *this.props.n} style = {{
                     width: el.width * 5,
                     height: Constants.DART_SIZE * 2,
                     position: 'absolute',
                     borderRadius: 10,
-                    backgroundColor: 'rgb(' + (255 - (el.width - 8) / 20 * 255).toString() +',0,' + ((el.width - 8) / 20 * 255).toString() + ')',
+                    backgroundColor: `rgb(${red}, 0, ${blue})`,
                     left: el.x * Constants.PSEUDO_PIXEL -  el.width * 2.5,
                     top: el.y * Constants.PSEUDO_PIXEL -  Constants.PSEUDO_PIXEL * 2.5,
                     zIndex: 3,
@@ -39,17 +43,15 @@ export default class Block extends Component<Interface> {
             );
         }).concat(blockList);
 
-        i++;
-
         return blockList;
     }
 
     render() {
         let blockList: any[] = [];
-
-        blockList.push(this.a());
-        blockList.push(this.a());
-        blockList.push(this.a());
+        
+        for(let i = 0; i < 3; i++) {
+            blockList.push(this.a(i));
+        }
 
         return(
             <View>
