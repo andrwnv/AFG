57871ac609a8367267a01cb1903651f8f274df7f
Andrew G <glazynovand@gmail.com>
2020-08-05 13:26:31 +0700

    [Tower game]: all modules on ts

--- components/games/tower/Axis.tsx
+++ components/games/tower/Axis.tsx
@@ -1,21 +1,19 @@
-import React, {Component} from "react";
-import {View, Image} from "react-native";
-import {Constants} from './Constants'
-import Svg, { Path, Rect, Line } from "react-native-svg";
+import React, { Component } from 'react';
+import { View }             from 'react-native';
+import Svg, { Line }        from 'react-native-svg';
+
+import { Constants }        from './Constants'
 
 
+export default class Block extends Component {
+    props: any;
 
-export default class Block extends Component{
-
-
-    constructor(props){
+    constructor(props: any) {
         super(props);
+        this.props = props;
     }
 
- 
-
-    render(){
-
+    render() {
         return(
             <View style = {{ position : 'absolute' , zIndex : -1}}>
                 <Svg height={Constants.MAX_HEIGHT.toString()} width={Constants.MAX_WIDTH.toString()} style = {{ position : 'absolute' , zIndex : -1}}>
@@ -30,8 +28,6 @@ export default class Block extends Component{
                     <Line x1={Constants.MAX_WIDTH/4*3} y1 = {70*Constants.PSEUDO_PIXEL} x2={Constants.MAX_WIDTH/4*3} y2 = {20*Constants.PSEUDO_PIXEL} stroke="black" strokeWidth="3"/>
                 </Svg>
             </View>
-        )
-        
-
+        );
     }
 }
