83f6845368ba2bf3c33c54d01707b86d962767ac
Andrew G <glazynovand@gmail.com>
2021-02-23 21:44:58 +0300

    minor fix

--- endpoints/EventTimersManager.ts
+++ endpoints/EventTimersManager.ts
@@ -9,29 +9,29 @@ export default class EventTimersManager {
     // @ts-ignore
     static initTimers(clearCallback: Callback, sleepCallback: Callback, moodCallback: Callback, eatCallback: Callback) {
 
-        // if (EventTimersManager.clearTimer == undefined && EventTimersManager.sleepTimer == undefined && EventTimersManager.moodTimer == undefined && EventTimersManager.eatTimer == undefined) {
-        //
-        //     console.log("[EventTimersManager] -> CREATE HERO PROPS TIMERS !!!");
-        //
-        //     EventTimersManager.clearTimer = setInterval(() => {
-        //         clearCallback();
-        //         console.log("CLEAR TIMER!")
-        //     }, 5 * 60 * 1000);
-        //
-        //     EventTimersManager.sleepTimer = setInterval(() => {
-        //         sleepCallback();
-        //         console.log("SLEEP TIMER!")
-        //     }, 6 * 60 * 1000);
-        //
-        //     EventTimersManager.moodTimer = setInterval(() => {
-        //         moodCallback();
-        //         console.log("MOOD TIMER!")
-        //     }, 2 * 60 * 1000);
-        //
-        //     EventTimersManager.eatTimer = setInterval(() => {
-        //         eatCallback();
-        //         console.log("EAT TIMER!")
-        //     }, 3 * 60 * 1000);
-        // }
+        if (EventTimersManager.clearTimer == undefined && EventTimersManager.sleepTimer == undefined && EventTimersManager.moodTimer == undefined && EventTimersManager.eatTimer == undefined) {
+
+            console.log("[EventTimersManager] -> CREATE HERO PROPS TIMERS !!!");
+
+            EventTimersManager.clearTimer = setInterval(() => {
+                clearCallback();
+                console.log("CLEAR TIMER!")
+            }, 5 * 60 * 1000);
+
+            EventTimersManager.sleepTimer = setInterval(() => {
+                sleepCallback();
+                console.log("SLEEP TIMER!")
+            }, 6 * 60 * 1000);
+
+            EventTimersManager.moodTimer = setInterval(() => {
+                moodCallback();
+                console.log("MOOD TIMER!")
+            }, 2 * 60 * 1000);
+
+            EventTimersManager.eatTimer = setInterval(() => {
+                eatCallback();
+                console.log("EAT TIMER!")
+            }, 3 * 60 * 1000);
+        }
     }
 }
